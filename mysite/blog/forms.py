4ade4e0e2ae84f70038798b90decd31e33cd496a
KimSeungHan-python <rlatmdgks4@khu.ac.kr>
2025-06-24 21:30:04 +0900

    Fix PostForm fields: use text and image instead of content

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -2,7 +2,11 @@
 from django import forms
 from .models import Post
 
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['title', 'content']
+# class PostForm(forms.ModelForm):
+#     class Meta:
+#         model = Post
+#         fields = ['title', 'content']
++    class Meta:
++        model = Post
++        # 'content' → 'text'로 변경, 이미지 업로드를 위한 'image'도 추가
++        fields = ('author', 'title', 'text', 'image',)
